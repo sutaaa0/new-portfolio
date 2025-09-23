0f3b25a192fef9bb4d2e6f1bf61d4445e4825c73
sutaaa0 <sutadev0@gmail.com>
2025-09-24 00:15:51 +0700

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,12 @@
 "use client";
 import { ContainerContent } from "./components/ContainerContent";
-import { Content } from "./components/Content";
-import SmoothScroll from "./components/SmoothScroll";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center scroll-smooth">
-        <div>
-          <ContainerContent />
-        </div>
+      <div>
+         <ContainerContent />
+      </div>
     </main>
   );
 }
