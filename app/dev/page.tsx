2cdd46661cb3ededf31c1faf418268989a6bfb38
sutaaa0 <sutadev0@gmail.com>
2025-09-24 00:16:05 +0700

    feat: create page.tsx

---
+++ app/dev/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import MinimalUnderConstruction from "../pages/Develop";
+
+const page = () => {
+  return (
+      <MinimalUnderConstruction />
+  );
+};
+
+export default page;
